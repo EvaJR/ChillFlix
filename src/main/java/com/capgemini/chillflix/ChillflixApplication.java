e20747f835748872c1a8cc2811f8658b9794e066
FunkyFeudalFox <arjan_tammer@yahoo.co.uk>
2018-07-30 16:19:10 +0200

    it's alive!

---
+++ src/main/java/com/capgemini/chillflix/ChillflixApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.chillflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChillflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChillflixApplication.class, args);
+	}
+}
