17a1da6fc17c2f40610d234ff3063f789edb7bec
Sanne <sanne.van.mourik@capgemini.com>
2018-08-03 16:33:28 +0200

    added post/get/put functions

--- Chillflix.web/ChillFlixApp/src/app/components/services/movie.service.ts
+++ Chillflix.web/ChillFlixApp/src/app/components/services/movie.service.ts
@@ -1,15 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
+import { Observable } from '../../../../node_modules/rxjs';
+import { Movie } from '../../models/movie';
+
+
 
 @Injectable()
 export class MovieService {
 
-  constructor(private http: HttpClient) { }
+  moviesUrl = 'http://localhost:8080/api/movies'; 
 
-  public getAll(){
 
-    return this.http.get("http://localhost:8080/api/movies");
+  constructor(private http: HttpClient) {
+ 
+   }
+
+
+  public getAll(): Observable<Movie[]>{
+    return this.http.get<Movie[]>(this.moviesUrl)
   }
 
+
+  addMovie (movie: Movie): Observable<Movie> {
+    return this.http.post<Movie>(this.moviesUrl, movie)
+
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteMovie (id: number): Observable<{}> {
+    const url = `${this.moviesUrl}/${id}`; // DELETE api/heroes/42
+    return this.http.delete(url)
+  }
+
+  /** PUT: update the hero on the server. Returns the updated hero upon success. */
+  updateMovie (movie: Movie): Observable<Movie> {
+    return this.http.put<Movie>(this.moviesUrl, movie)
+
+  }
 }
