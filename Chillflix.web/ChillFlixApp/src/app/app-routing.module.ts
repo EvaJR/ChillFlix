4c5b082c9ca10004754f47601839ae20dee172b8
Eva van Rooden <evavanrooden@gmail.com>
2018-08-09 16:53:57 +0200

    added home page

--- Chillflix.web/ChillFlixApp/src/app/app-routing.module.ts
+++ Chillflix.web/ChillFlixApp/src/app/app-routing.module.ts
@@ -5,11 +5,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { MovieListComponent} from '../app/components/movie-list/movie-list.component'
 import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { CardComponent } from './components/card/card.component';
 
 const routes: Routes = [
   { path: 'movie-list', component: MovieListComponent },
-  {path: 'home', component: HomeComponent},
-
+  { path: 'home', component: HomeComponent },
+  { path: 'featured', component: CarouselComponent },
+  { path: 'movie-cards', component: CardComponent },
   { path: 'admin',
     component: AdminComponent,
     canActivate: [AdminGuard]
